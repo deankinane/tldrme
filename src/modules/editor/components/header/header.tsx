c0271414e510e56140fdc70fdbd540f534f44877
Dean <dean.kinane@gmail.com>
2022-11-30 23:35:40 +0000

    Changed portfolio pic alignment

--- src/modules/editor/components/header/header.tsx
+++ src/modules/editor/components/header/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
 	const { resume } = useContext(ResumeContext)
 	return (
 		<div className="flex h-36 lg:h-96">
-			<div className="flex h-full min-w-fit items-center justify-center md:w-80 lg:w-96 lg:min-w-[24rem]">
+			<div className="flex h-full min-w-fit items-center md:w-80 lg:w-96 lg:min-w-[24rem]">
 				<ProfilePicture imageUrl={resume.profilePicUrl || ''} />
 			</div>
 			<div className="flex-grow pl-4 md:pl-12">
