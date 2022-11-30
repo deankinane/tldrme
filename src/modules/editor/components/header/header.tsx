d4699f258d08291eaa091004ce8014b528d7aa7c
Dean <dean.kinane@gmail.com>
2022-11-30 22:13:29 +0000

    independant column scrolling on mobile devices

--- src/modules/editor/components/header/header.tsx
+++ src/modules/editor/components/header/header.tsx
@@ -6,7 +6,7 @@ import ProfilePicture from './profile-picture'
 export default function Header() {
 	const { resume } = useContext(ResumeContext)
 	return (
-		<div className="flex h-52 lg:h-96">
+		<div className="md:52 flex h-48 lg:h-96">
 			<div className="flex h-full min-w-fit items-center lg:w-96 lg:min-w-[24rem]">
 				<ProfilePicture imageUrl={resume.profilePicUrl || ''} />
 			</div>
--- src/modules/editor/editor.tsx
+++ src/modules/editor/editor.tsx
@@ -14,18 +14,18 @@ export default function Editor({ resume }: Props) {
 
 	return (
 		<ResumeProvider initialState={resume}>
-			<div className="min-h-full px-8 shadow-lg lg:px-16">
+			<div className="h-full overflow-hidden px-8 shadow-lg md:overflow-auto lg:px-16">
 				<Header />
 
 				<div
 					className={`flex w-full ${
 						smallScreen
-							? 'relative snap-x snap-mandatory overflow-x-auto'
+							? 'fixed top-48 bottom-0 right-0 left-0 snap-x snap-mandatory overflow-x-auto'
 							: 'flex-row'
 					}`}
 				>
 					<div
-						className={`w-full md:w-96 ${
+						className={`w-full overflow-y-scroll px-4 md:w-96 md:px-0 ${
 							smallScreen ? 'shrink-0 snap-center' : ''
 						}`}
 					>
@@ -33,7 +33,7 @@ export default function Editor({ resume }: Props) {
 					</div>
 
 					<div
-						className={`w-full md:pl-12 lg:w-auto lg:flex-grow lg:pl-16 ${
+						className={`h-full w-full  overflow-y-scroll px-4 md:px-0 md:pl-12 lg:w-auto lg:flex-grow lg:pl-16 ${
 							smallScreen ? 'shrink-0 snap-center' : ''
 						}`}
 					>
