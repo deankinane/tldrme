179b6af168e33443c5075d7eb9100c88bd9b2195
Dean <dean.kinane@gmail.com>
2022-11-30 21:33:00 +0000

    Added horizontal scroll to switch between columns on mobile display

--- src/modules/editor/editor.tsx
+++ src/modules/editor/editor.tsx
@@ -11,52 +11,38 @@ interface Props {
 }
 export default function Editor({ resume }: Props) {
 	const smallScreen = useSmallScreen()
-	const [currentColumn, setCurrentColumn] = useState(0)
-
-	const switchColumn = useCallback(() => {
-		const newCol = (currentColumn + 1) % 2
-		console.log('New Col', newCol)
-		setCurrentColumn(newCol)
-	}, [currentColumn])
 
 	return (
 		<ResumeProvider initialState={resume}>
 			<div className="min-h-full px-8 shadow-lg lg:px-16">
 				<Header />
 
-				<div className="flex flex-row">
-					{!smallScreen || (smallScreen && currentColumn === 0) ? (
-						<div className="w-full md:w-96">
-							<EditorColumn columnIndex={0} />
-						</div>
-					) : (
-						<></>
-					)}
+				<div
+					className={`flex w-full ${
+						smallScreen
+							? 'relative snap-x snap-mandatory overflow-x-auto'
+							: 'flex-row'
+					}`}
+				>
+					<div
+						className={`w-full md:w-96 ${
+							smallScreen ? 'shrink-0 snap-center' : ''
+						}`}
+					>
+						<EditorColumn columnIndex={0} />
+					</div>
 
-					{!smallScreen || (smallScreen && currentColumn === 1) ? (
-						<div className="w-full md:pl-12 lg:w-auto lg:flex-grow lg:pl-16">
-							<EditorColumn columnIndex={1} />
-						</div>
-					) : (
-						<></>
-					)}
+					<div
+						className={`w-full md:pl-12 lg:w-auto lg:flex-grow lg:pl-16 ${
+							smallScreen ? 'shrink-0 snap-center' : ''
+						}`}
+					>
+						<EditorColumn columnIndex={1} />
+					</div>
 				</div>
 			</div>
 
 			<TrashDropTarget />
-
-			{smallScreen ? (
-				<div className="fixed bottom-4 left-4">
-					<button
-						className="h-8 w-8 rounded-md bg-cyan-600"
-						onClick={switchColumn}
-					>
-						O
-					</button>
-				</div>
-			) : (
-				<></>
-			)}
 		</ResumeProvider>
 	)
 }
