0685d2cd4ab190add2cceb2f266482739c45239a
Dean Kinane <dean.kinane@gmail.com>
2022-12-14 22:59:39 +0000

    Finished initial implementation of profile picture cropper

--- src/modules/editor/components/header/cropper-modal.tsx
+++ src/modules/editor/components/header/cropper-modal.tsx
@@ -6,20 +6,32 @@ import AvatarEditor from 'react-avatar-editor'
 
 interface Props {
 	image: string
+	onImageCropped: (data: string) => void
 }
 
-export default function CropperModal({ image }: Props) {
+export default function CropperModal({ image, onImageCropped }: Props) {
 	const isMd = useMediaQuery(`(min-width: ${MediaSize.md})`)
 	const [zoom, setZoom] = useState(1.0)
-	const editor = useRef(null)
+	const editor = useRef<AvatarEditor | null>(null)
 
 	const onZoomChanged = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
 		setZoom(parseFloat(ev.currentTarget.value))
 	}, [])
 
+	const onClickSave = useCallback(
+		() => {
+			if (editor.current) {
+				const canvas = editor.current.getImage()
+				const dataUrl = canvas.toDataURL()
+				onImageCropped(dataUrl)
+			}
+		},
+		[onImageCropped],
+	)
+
 	return (
 		<div className="fixed top-0 bottom-0 left-0 right-0 z-10 bg-[#000000cc]">
-			<div className="absolute top-1/3 left-1/2 w-fit -translate-y-1/2 -translate-x-1/2 rounded-md bg-white shadow-md">
+			<div className="absolute top-1/3 left-1/2 w-fit -translate-y-1/2 -translate-x-1/2 rounded-md bg-white shadow-lg shadow-black">
 				<AvatarEditor
 					ref={editor}
 					image={image}
@@ -27,17 +39,24 @@ export default function CropperModal({ image }: Props) {
 					height={isMd ? 500 : 200}
 					scale={zoom}
 				/>
-				<div className="px-4 py-3">
-					<input
-						className="w-full"
-						type="range"
-						min="1"
-						max="2"
-						step="0.1"
-						value={zoom}
-						onChange={onZoomChanged}
-					/>
-					<button>Save</button>
+				<div className="px-8 py-6">
+					<div className="flex w-full items-center justify-center">
+						<p className="mr-4 text-sm font-bold">Zoom</p>
+						<input
+							className="w-full"
+							type="range"
+							min="1"
+							max="2"
+							step="0.1"
+							value={zoom}
+							onChange={onZoomChanged}
+						/>
+						<button
+							onClick={onClickSave}
+							className="ml-8 rounded-md bg-purple-700 px-4 py-1 font-semibold text-white">
+							Save
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/modules/editor/components/header/profile-picture.tsx
+++ src/modules/editor/components/header/profile-picture.tsx
@@ -26,16 +26,9 @@ export default function ProfilePicture({ imageData }: Props) {
 				if (!file) return
 
 				const reader = new FileReader()
-				reader.onloadend = function () {
+				reader.onloadend = function() {
 					const result = reader.result?.toString()
 					if (result) {
-						// mUpdateProfilePicture.mutate({
-						// 	resumeId: resume.id,
-						// 	base64: result,
-						// })
-
-						// resume.profilePicUrl = result
-						// updateResume(resume)
 						setCropImageData(result)
 						setCropMode(true)
 					}
@@ -44,6 +37,19 @@ export default function ProfilePicture({ imageData }: Props) {
 			}
 		},
 		[]
+	)
+
+	const onImageCropped = useCallback(
+		(data: string) => {
+			setCropMode(false)
+			resume.profilePicUrl = data
+			updateResume(resume)
+			mUpdateProfilePicture.mutate({
+				resumeId: resume.id,
+				base64: data,
+			})
+		},
+		[resume, updateResume],
 	)
 
 	return (
@@ -77,7 +83,7 @@ export default function ProfilePicture({ imageData }: Props) {
 					className="h-full w-full rounded-full bg-slate-400 transition-opacity group-hover:opacity-50"
 				></div>
 			)}
-			{cropMode ? <CropperModal image={cropImageData} /> : <></>}
+			{cropMode ? <CropperModal image={cropImageData} onImageCropped={onImageCropped} /> : <></>}
 		</div>
 	)
 }
