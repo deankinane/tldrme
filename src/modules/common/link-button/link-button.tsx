69050afe46161bc83dbd525ede5a5863b594824f
Dean Kinane <dean.kinane@gmail.com>
2022-12-23 15:04:46 +0000

    Fixed issue with link button size

--- src/modules/common/link-button/link-button.tsx
+++ src/modules/common/link-button/link-button.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../button/button'
-interface Props
-    extends React.ComponentProps<'button'> {
-    href: string
+interface Props extends React.ComponentProps<typeof Link> {
+	href: string
 }
-export const LinkButton = ({ href, ...props }: Props) => {
-    return (
-        <Link href={href}>
-            <Button {...props}>
-                {props.children}
-            </Button>
-
-        </Link>
-    )
+export const LinkButton = (props: React.ComponentProps<typeof Link>) => {
+	return (
+		<Link
+			{...props}
+			className={`${props.className} rounded-md bg-indigo-600 py-4 px-8 font-semibold text-white shadow-2xl transition-colors duration-500 hover:bg-indigo-500`}
+		>
+			{props.children}
+		</Link>
+	)
 }
