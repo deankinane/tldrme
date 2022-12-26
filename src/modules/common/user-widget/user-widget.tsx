2c02019e4121fa7bf13e361660ce8f8d07cfefcb
Dean Kinane <dean.kinane@gmail.com>
2022-12-26 00:36:38 +0000

    Updated design of user widget

--- src/modules/common/user-widget/user-widget.tsx
+++ src/modules/common/user-widget/user-widget.tsx
@@ -1,4 +1,9 @@
-import { ArrowLeftOnRectangleIcon, UserIcon } from '@heroicons/react/24/solid'
+import {
+	ArrowLeftOnRectangleIcon,
+	UserIcon,
+	XCircleIcon,
+	XMarkIcon,
+} from '@heroicons/react/24/solid'
 import { useSession } from 'next-auth/react'
 import React, { useState } from 'react'
 
@@ -9,16 +14,22 @@ export const UserWidget = (props: React.ComponentProps<'div'>) => {
 	return status === 'authenticated' ? (
 		<div
 			{...props}
-			className={`${props.className} flex items-center rounded-3xl bg-indigo-700 p-2 text-sm font-thin text-white`}
+			className={`${props.className} flex items-center rounded-3xl bg-[#00000088] p-2 text-sm font-thin text-white`}
 		>
-			<div className="rounded-full" onClick={() => setOpen((o) => !o)}>
+			<div className="rounded-full" onClick={() => setOpen(true)}>
 				<UserIcon className="w-5" />
 			</div>
 			{open ? (
-				<div className="absolute right-0 top-12 flex items-center rounded-md bg-indigo-700 p-2 pl-4 shadow-lg">
+				<div className="absolute -right-2 -top-2 flex items-center rounded-md bg-[#00000088] p-2 pl-4 shadow-lg">
 					<p className="grow">{session.user?.email}</p>
-					<div className="ml-4 rounded-full bg-indigo-500 p-2">
+					<div className="ml-4 rounded-full bg-indigo-600 p-2">
 						<ArrowLeftOnRectangleIcon className="w-5" />
+					</div>
+					<div
+						className="ml-2 rounded-full bg-indigo-600 p-2"
+						onClick={() => setOpen(false)}
+					>
+						<XMarkIcon className="w-5" />
 					</div>
 				</div>
 			) : (
