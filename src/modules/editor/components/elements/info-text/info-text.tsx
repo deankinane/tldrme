2a0fe621d1196ae27b839d1e52a1bb8e376f377b
Dean Kinane <dean.kinane@gmail.com>
2023-01-05 11:59:50 +0000

    Created info text element

---
+++ src/modules/editor/components/elements/info-text/info-text.tsx
@@ -0,0 +1,39 @@
+import { ElementType } from '@prisma/client'
+import React, { useCallback } from 'react'
+import EditText from '../../edit-text/edit-text'
+import { FgColors } from '../../sidebar/components/color-picker/color-picker'
+import type { ElementProps } from '../common/element-props'
+
+export default function InfoTextElement({
+	element,
+	onElementUpdated,
+	styles,
+	onBlur,
+}: ElementProps) {
+	const onTextChanged = useCallback(
+		(text: string) => {
+			if (element.text === text) return
+			element.text = text
+			onElementUpdated(element)
+		},
+		[element, onElementUpdated]
+	)
+
+	return (
+		<div
+			className="mb-2 flex "
+			data-testid={`element-${ElementType.InfoText}`}
+		>
+			<EditText
+				fontStyles={`${
+					FgColors[styles.elementTextColor]
+				} text-xs lg:text-sm`}
+				onTextChanged={onTextChanged}
+				text={element.text}
+				className="grow"
+				multiline
+				onBlur={onBlur}
+			/>
+		</div>
+	)
+}
